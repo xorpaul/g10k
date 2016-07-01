898b88b513733b99786654c3367e80d1791ec425
Andreas Paul <andreas.paul@1und1.de>
2016-07-01 16:32:55 +0200

    first successful tests

--- g10k_test.go
+++ g10k_test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-    "fmt"
+		"regexp"
 		"testing"
 )
 
 
 func TestPreparePuppetfile(t *testing.T) {
-  fmt.Println(preparePuppetfile("tests/TestPreparePuppetfile"))
+	expected := regexp.MustCompile("(moduledir 'external_modules'\nmod 'puppetlabs/ntp')")
+	got := preparePuppetfile("tests/TestPreparePuppetfile")
+
+	if ! expected.MatchString(got) {
+		t.Error("Expected", expected, "got", got)
+	}
 }
