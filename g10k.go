3c963a20b439be77110f4da2b6624a03f433e82a
Andreas Paul <xorpaul@gmail.com>
2018-06-25 18:47:36 +0200

    bump version to v0.4.9

--- g10k.go
+++ g10k.go
@@ -186,7 +186,7 @@ func main() {
 	version := *versionFlag
 
 	if version {
-		fmt.Println("g10k version 0.4.8 Build time:", buildtime, "UTC")
+		fmt.Println("g10k version 0.4.9 Build time:", buildtime, "UTC")
 		os.Exit(0)
 	}
 
