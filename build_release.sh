b689b4504c28a439f06e62168b2a6b1745ee0a85
Andreas Paul <xorpaul@gmail.com>
2020-07-01 12:59:56 +0200

    use generic build script

--- build_release.sh
+++ build_release.sh
@@ -16,8 +16,11 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-sed -i "s/g10k version [^ ]*/g10k version ${1}/" g10k.go
-git add g10k.go
+# try to get the project name from the current working directory
+projectname=${PWD##*/}
+
+sed -i "s/${projectname} version [^ ]*/${projectname} version ${1}/" ${projectname}.go
+git add ${projectname}.go
 git commit -m "bump version to v${1}"
 
 echo "creating git tag v${1}"
@@ -27,41 +30,41 @@ git push -f --tags
 git push
 
 echo "creating github release v${1}"
-github-release release  --user xorpaul     --repo g10k     --tag v${1}     --name "v${1}"     --description "${2}"
+github-release release  --user xorpaul     --repo ${projectname}     --tag v${1}     --name "v${1}"     --description "${2}"
 
 upx=`which upx`
 
 if [ ${#upx} -gt 0 ]; then
-  echo "building and uploading g10k-darwin-amd64-debug"
+  echo "building and uploading ${projectname}-darwin-amd64-debug"
   BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.buildtime=$BUILDTIME v${1}" && date
-  zip g10k-darwin-amd64-debug.zip g10k
-  github-release upload     --user xorpaul     --repo g10k     --tag v${1}     --name "g10k-darwin-amd64-debug.zip" --file g10k-darwin-amd64-debug.zip
+  zip ${projectname}-darwin-amd64-debug.zip ${projectname}
+  github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-darwin-amd64-debug.zip" --file ${projectname}-darwin-amd64-debug.zip
 fi
 
-echo "building and uploading g10k-darwin-amd64"
+echo "building and uploading ${projectname}-darwin-amd64"
 BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X main.buildtime=$BUILDTIME v${1}" && date
 if [ ${#upx} -gt 0 ]; then
-  $upx --brute g10k
+  $upx --brute ${projectname}
 fi
-zip g10k-darwin-amd64.zip g10k
-github-release upload     --user xorpaul     --repo g10k     --tag v${1}     --name "g10k-darwin-amd64.zip" --file g10k-darwin-amd64.zip
+zip ${projectname}-darwin-amd64.zip ${projectname}
+github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-darwin-amd64.zip" --file ${projectname}-darwin-amd64.zip
 
 
 
 
 
 if [ ${#upx} -gt 0 ]; then
-  echo "building and uploading g10k-linux-amd64-debug"
-  BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && go build -race -ldflags "-X main.buildtime=$BUILDTIME v${1}" && date && env g10k_cachedir=/tmp/g10k ./g10k -config test.yaml -branch benchmark 2>&1
-  zip g10k-linux-amd64-debug.zip g10k
-  github-release upload     --user xorpaul     --repo g10k     --tag v${1}     --name "g10k-linux-amd64-debug.zip" --file g10k-linux-amd64-debug.zip
+  echo "building and uploading ${projectname}-linux-amd64-debug"
+  BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && go build -race -ldflags "-X main.buildtime=$BUILDTIME v${1}" && date && env ${projectname}_cachedir=/tmp/${projectname} ./${projectname} -config test.yaml -branch benchmark 2>&1
+  zip ${projectname}-linux-amd64-debug.zip ${projectname}
+  github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-linux-amd64-debug.zip" --file ${projectname}-linux-amd64-debug.zip
 fi
 
-echo "building and uploading g10k-linux-amd64"
-BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && go build -race -ldflags "-s -w -X main.buildtime=$BUILDTIME v${1}" && date && env g10k_cachedir=/tmp/g10k ./g10k -config test.yaml -branch benchmark 2>&1
+echo "building and uploading ${projectname}-linux-amd64"
+BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && go build -race -ldflags "-s -w -X main.buildtime=$BUILDTIME v${1}" && date && env ${projectname}_cachedir=/tmp/${projectname} ./${projectname} -config test.yaml -branch benchmark 2>&1
 if [ ${#upx} -gt 0 ]; then
-  $upx --brute g10k
+  $upx --brute ${projectname}
 fi
-zip g10k-linux-amd64.zip g10k
-github-release upload     --user xorpaul     --repo g10k     --tag v${1}     --name "g10k-linux-amd64.zip" --file g10k-linux-amd64.zip
+zip ${projectname}-linux-amd64.zip ${projectname}
+github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-linux-amd64.zip" --file ${projectname}-linux-amd64.zip
 
