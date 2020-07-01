b7162f190f4f54c06ee091864d4987f8a7bc75e2
Andreas Paul <xorpaul@gmail.com>
2020-07-01 16:14:17 +0200

    fix main.buildtime as single string

--- build_release.sh
+++ build_release.sh
@@ -36,13 +36,13 @@ upx=`which upx`
 
 if [ ${#upx} -gt 0 ]; then
   echo "building and uploading ${projectname}-darwin-amd64-debug"
-  BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.buildtime=$BUILDTIME v${1}" && date
+  BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.buildtime=$BUILDTIME_v${1}" && date
   zip ${projectname}-darwin-amd64-debug.zip ${projectname}
   github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-darwin-amd64-debug.zip" --file ${projectname}-darwin-amd64-debug.zip
 fi
 
 echo "building and uploading ${projectname}-darwin-amd64"
-BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X main.buildtime=$BUILDTIME v${1}" && date
+BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X main.buildtime=$BUILDTIME_v${1}" && date
 if [ ${#upx} -gt 0 ]; then
   $upx --brute ${projectname}
 fi
@@ -55,13 +55,13 @@ github-release upload     --user xorpaul     --repo ${projectname}     --tag v${
 
 if [ ${#upx} -gt 0 ]; then
   echo "building and uploading ${projectname}-linux-amd64-debug"
-  BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && go build -race -ldflags "-X main.buildtime=$BUILDTIME v${1}" && date && env ${projectname}_cachedir=/tmp/${projectname} ./${projectname} -config test.yaml -branch benchmark 2>&1
+  BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && go build -race -ldflags "-X main.buildtime=$BUILDTIME_v${1}" && date && env ${projectname}_cachedir=/tmp/${projectname} ./${projectname} -config test.yaml -branch benchmark 2>&1
   zip ${projectname}-linux-amd64-debug.zip ${projectname}
   github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-linux-amd64-debug.zip" --file ${projectname}-linux-amd64-debug.zip
 fi
 
 echo "building and uploading ${projectname}-linux-amd64"
-BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && go build -race -ldflags "-s -w -X main.buildtime=$BUILDTIME v${1}" && date && env ${projectname}_cachedir=/tmp/${projectname} ./${projectname} -config test.yaml -branch benchmark 2>&1
+BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') && go build -race -ldflags "-s -w -X main.buildtime=$BUILDTIME_v${1}" && date && env ${projectname}_cachedir=/tmp/${projectname} ./${projectname} -config test.yaml -branch benchmark 2>&1
 if [ ${#upx} -gt 0 ]; then
   $upx --brute ${projectname}
 fi
